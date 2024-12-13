aad480e6757c15449ca9208cdc4ec28eeb5a045c
Jackson Uhl <jackson.g.uhl@gmail.com>
2024-12-13 18:59:14 -0500

    Add layout styling

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
+import { layoutStyling } from "./Layout.module.css";
+import ClassnameJoiner from "../../utilities/helpers/ClassnameJoiner";
+import { justifySelfCenter } from "../../styling/shared.module.css";
 
 export default function Layout() {
     return (
-        <div>
+        <div className={ClassnameJoiner.join([layoutStyling])}>
             <HeaderComponent />
             <Outlet />
         </div>
