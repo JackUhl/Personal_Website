c33eb1040384abbe283a21d7a1e37ed663e228bb
Jackson Uhl <jackson.g.uhl@gmail.com>
2025-01-21 00:13:23 -0500

    Resume work experience

---
+++ src/models/objects/ResumeItem.ts
@@ -0,0 +1,7 @@
+export interface ResumeItem {
+    company: string;
+    start: Date;
+    end?: Date;
+    position: string;
+    location: string;
+}
--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -1,5 +1,51 @@
+import { useContext } from "react";
+import { Months } from "../../models/enums/months"
+import { ResumeItem } from "../../models/objects/ResumeItem"
+import { bulletPoint, bulletPointConnector, companyName, desktopContainer, divider, mobileContainer, resumeStyling, sectionTitle } from "./Resume.module.css"
+import { IsMobileContext } from "../../contexts/IsMobileContext";
+import { alignItemsCenter, flexRow } from "../../styling/shared.module.css";
+import ClassnameJoiner from "../../utilities/helpers/ClassnameJoiner";
+
 export default function Resume() {
+    const resumeItems: ResumeItem[] = [
+        {
+            company: "Total Quality Logistics",
+            start: new Date(2023, 5, 1),
+            position: "Software Developer",
+            location: "Cincinnati, OH"
+        },
+        {
+            company: "Bowling Green State University",
+            start: new Date(2022, 7, 1),
+            end: new Date(2022, 11, 1),
+            position: "Learning Assistant",
+            location: "Bowling Green, OH"
+        },
+    ]
+    
+    const isMobile = useContext(IsMobileContext);
+
+    const renderDate = (date?: Date) => {
+        return (date ? Months[date.getMonth()] + " " + date.getFullYear() : "Present")
+    }
+
     return (
-        <p>Resume Page</p>
+        <div className={resumeStyling}>
+            <div className={isMobile ? mobileContainer : desktopContainer}>
+                <p className={sectionTitle}>Work Experience</p>
+                {resumeItems.map((resumeItem, index) => 
+                    <div>
+                        <div key={index} className={ClassnameJoiner.join([flexRow, alignItemsCenter])}>
+                            <div className={bulletPoint}/>
+                            <div>
+                                <p><span className={companyName}>{resumeItem.company},</span> <span>{resumeItem.location}</span></p>
+                                <p>{renderDate(resumeItem.start)} - {renderDate(resumeItem.end)} <span className={divider}>|</span> {resumeItem.position}</p>
+                            </div>
+                        </div>
+                        {index != resumeItems.length - 1 && <div className={bulletPointConnector}/>}
+                    </div>
+                )}
+            </div>
+        </div>
     )
 }
