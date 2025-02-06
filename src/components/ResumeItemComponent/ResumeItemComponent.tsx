95079a6091cd07804de805af7beba42a430a565b
Jackson Uhl <jackson.g.uhl@gmail.com>
2025-02-05 19:37:56 -0500

    Reconfigure styling

--- src/components/ResumeItemComponent/ResumeItemComponent.tsx
+++ src/components/ResumeItemComponent/ResumeItemComponent.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react"
 import { Months } from "../../models/enums/Months"
-import { bulletPoint, bulletPointConnector, mainText, divider, description, resumeItem, expander, smallGap, resumeItemComponent, rotateExpanded, rotateCollapsed } from "./ResumeItemComponent.module.css"
+import { bulletPoint, bulletPointConnector, mainText, divider, description, resumeItem, expander, resumeItemComponent, rotateExpanded, rotateCollapsed, resumeItemTitle } from "./ResumeItemComponent.module.css"
 import { alignItemsCenter, flexColumn, flexRow } from "../../styling/shared.module.css"
 import ClassnameJoiner from "../../utilities/helpers/ClassnameJoiner"
 import IResumeItemComponent from "./IResumeItemComponent"
 import expanderIcon from "../../assets/svg/expand.svg"
 
 export default function ResumeItemComponent(props: IResumeItemComponent) {
-    const [showDescription, setShowDescription] = useState(false);
+    const [expanded, setExpanded] = useState(true);
 
     const renderDate = (date?: Date) => {
         return (date ? Months[date.getMonth()] + " " + date.getFullYear() : "Present")
     }
 
     const handleResumeItemClick = () => {
-        setShowDescription(!showDescription);
+        setExpanded(!expanded);
     }
     
     return (
@@ -24,15 +24,15 @@ export default function ResumeItemComponent(props: IResumeItemComponent) {
                     <div className={bulletPoint}/>
                     {!props.lastItem && <div className={bulletPointConnector}/>}
                 </div>
-                <div className={resumeItem} onClick={handleResumeItemClick}>
-                    <div className={ClassnameJoiner.join([flexRow, smallGap])}>
+                <div className={resumeItem}>
+                    <div className={ClassnameJoiner.join([flexRow, resumeItemTitle])} onClick={handleResumeItemClick}>
                         <div>
                             <p><span className={mainText}>{props.resumeItem.mainText},</span> <span>{props.resumeItem.subText}</span></p>
                             <p>{renderDate(props.resumeItem.start)} - {renderDate(props.resumeItem.end)}{props.resumeItem.position && <span><span className={divider}> | </span>{props.resumeItem.position}</span>}</p>
                         </div>
-                        <img className={ClassnameJoiner.join([expander, showDescription ? rotateExpanded : rotateCollapsed])} src={expanderIcon}/>
+                        <img className={ClassnameJoiner.join([expander, expanded ? rotateExpanded : rotateCollapsed])} src={expanderIcon}/>
                     </div>
-                    {showDescription && <p className={description}>{props.resumeItem.description}</p>}
+                    {expanded && <p className={description}>{props.resumeItem.description}</p>}
                 </div>
             </div>
         </div>
