f4f26b81b709483548358dadb4a430acc34e8b96
JackUhl <jackson.g.uhl@gmail.com>
2025-03-06 00:29:10 -0500

    Add resize to blog card

--- src/components/BlogCardComponent/BlogCardComponent.tsx
+++ src/components/BlogCardComponent/BlogCardComponent.tsx
@@ -1,13 +1,31 @@
-import { flexRow } from "../../styling/shared.module.css";
+import { useEffect, useRef, useState } from "react";
+import { flexColumn, flexRow } from "../../styling/shared.module.css";
 import ClassnameJoiner from "../../utilities/helpers/ClassnameJoiner";
 import DateRenderer from "../../utilities/helpers/DateRenderer";
-import { cardContainer, cardContent, cardContentDescription, cardContentTitle, cardImage, underlineItem } from "./BlogCardComponent.module.css";
+import { cardContainer, cardContent, cardContentDescription, cardContentTitle, cardImageSide, cardImageStacked, underlineItem } from "./BlogCardComponent.module.css";
 import { IBlogCardComponent } from "./IBlogCardComponent";
 
+const stackedThreshold = 525;
+
 export default function BlogCardComponent(props: IBlogCardComponent) {
+    const [stackedView, setStackedView] = useState(false);
+    const componentRef = useRef(null);
+
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver(() => {
+          if (componentRef.current) {
+            setStackedView(componentRef.current.offsetWidth <= stackedThreshold);
+          }
+        });
+    
+        if (componentRef.current) {
+          resizeObserver.observe(componentRef.current);
+        }
+      }, []);
+    
     return (
-        <div className={ClassnameJoiner.join([flexRow, cardContainer])}>
-            <img className={cardImage} src={props.blogItem.primaryImage} />
+        <div ref={componentRef} className={ClassnameJoiner.join([stackedView ? flexColumn : flexRow, cardContainer])}>
+            <img className={stackedView ? cardImageStacked : cardImageSide} src={props.blogItem.primaryImage} />
             <div className={cardContent}>
                 <p className={cardContentTitle}>{props.blogItem.title}</p>
                 <p className={underlineItem}>{DateRenderer.renderPartialDate(props.blogItem.createdDate)}</p>
