06da8d384a96f1672ea0a9370bd1bd5e8e3bb4ee
JackUhl <jackson.g.uhl@gmail.com>
2025-03-12 23:22:26 -0400

    Fix hamburger mobile menu

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,12 +1,13 @@
 import { Link, useLocation } from "react-router-dom";
 import { NavItems } from "../../models/constants/NavBarConstants";
-import { alignItemsCenter, alignItemsEnd, columnGap, flexColumn, flexRow, justifyContentBetween } from "../../styling/shared.module.css";
+import { alignItemsCenter, columnGap, flexColumn, flexRow, justifyContentAround, justifyContentBetween } from "../../styling/shared.module.css";
 import ClassnameJoiner from "../../utilities/helpers/ClassnameJoiner";
-import { collapseState, expandedState, hamburgerMenuStyle, headerBar, mobileMenu, name, navigationItem, navigationItemSelectable, navigationItemSelected } from "./HeaderComponent.module.css";
+import { collapseState, expandedState, hamburgerMenuStyle, headerBar, headerTitle, mobileItemsBox, mobileMenu, name, navigationItem, navigationItemSelectable, navigationItemSelected } from "./HeaderComponent.module.css";
 import { HomeRoute } from "../../models/constants/InternalUrlConstants";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { IsMobileContext } from "../../contexts/IsMobileContext";
 import burgerMenu from "../../assets/svg/burgerMenu.svg"
+import RevealComponent from "../RevealComponent/RevealComponent";
 
 export default function HeaderComponent() {
     const [mobileMenuExpanded, setMobileMenuExpanded] = useState(false)
@@ -14,44 +15,50 @@ export default function HeaderComponent() {
     const location = useLocation();
     const isMobile = useContext(IsMobileContext);
 
-    const shouldAddSelectedClass = (itemPathName: string) => {
-        let sitePathName = location.pathname;
-
-        return sitePathName == itemPathName ? navigationItemSelected : "";
-    }
-    
-    const handleNavBarItemClicked = () => {
-        window.scrollTo(0, 0);
-        setMobileMenuExpanded(false);
-    }
-
-    const handleHamburgerIconClick = () => {
-        setMobileMenuExpanded(!mobileMenuExpanded)
-    }
-
-    const nonMobileNav = () => {
-        return (
-            <div className={ClassnameJoiner.join([flexRow, alignItemsCenter, columnGap])}>
-                {NavItems.map((navItem, index) => {
-                    return (
-                        <div key={index}>
-                            <Link to={navItem.route} onClick={handleNavBarItemClicked} className={ClassnameJoiner.join([navigationItem, navigationItemSelectable, shouldAddSelectedClass(navItem.route)])}>{navItem.title}</Link>
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    }
-
     const hamburgerMenu = () => {
         return (
             <img src={burgerMenu} onClick={handleHamburgerIconClick} className={hamburgerMenuStyle}/>
         );
-    }
+    };
 
-    const mobileNav = () => {
+    const shouldAddSelectedClass = (itemPathName: string) => {
+        let sitePathName = location.pathname;
+
+        return sitePathName == itemPathName ? navigationItemSelected : "";
+    };
+    
+    const handleNavBarItemClicked = () => {
+        window.scrollTo(0, 0);
+        setMobileMenuExpanded(false);
+    };
+
+    const handleHamburgerIconClick = () => {
+        setMobileMenuExpanded(!mobileMenuExpanded)
+    };
+
+    const nonMobileStyle = useMemo(() => {
+        return ClassnameJoiner.join([flexRow, alignItemsCenter, columnGap]);
+    }, []);
+
+    const mobileStyle = useMemo(() => {
+        return ClassnameJoiner.join([flexColumn, alignItemsCenter, justifyContentAround, mobileItemsBox]);
+    }, []);
+
+    useEffect(() => {
+        setMobileMenuExpanded(false);
+    }, [isMobile]);
+
+    useEffect(() => {
+        if (mobileMenuExpanded) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    }, [mobileMenuExpanded]);
+
+    const navItems = () => {
         return (
-            <div className={ClassnameJoiner.join([flexColumn, alignItemsEnd])}>
+            <div className={isMobile ? mobileStyle : nonMobileStyle}>
                 {NavItems.map((navItem, index) => {
                     return (
                         <div key={index}>
@@ -60,27 +67,21 @@ export default function HeaderComponent() {
                     );
                 })}
             </div>
-        );
+        )
     }
 
-    useEffect(() => {
-        setMobileMenuExpanded(false);
-    }, [isMobile])
-
     return (
-        <>
-            <div className={ClassnameJoiner.join([flexRow, alignItemsCenter, justifyContentBetween])}>
-                <Link to={HomeRoute} className={navigationItem}>
-                    <div className={headerBar}>
-                        <p className={name}>Jackson Uhl</p>
-                        <p>Software Developer</p>
-                    </div>
-                </Link>
-                {isMobile ? hamburgerMenu() : nonMobileNav()}
-            </div>
+        <div className={ClassnameJoiner.join([flexRow, alignItemsCenter, justifyContentBetween, headerBar])}>
+            <Link to={HomeRoute} className={navigationItem}>
+                <div className={headerTitle}>
+                    <p className={name}>Jackson Uhl</p>
+                    <p>Software Developer</p>
+                </div>
+            </Link>
+            {isMobile ? hamburgerMenu() : navItems()}
             <div className={ClassnameJoiner.join([mobileMenu, mobileMenuExpanded ? expandedState : collapseState])}>
-                { mobileNav() }
+                { navItems() }
             </div>
-        </>
+        </div>
     )
 }
