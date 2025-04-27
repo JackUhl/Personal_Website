2440527eac5d095a2d0caefba9f1a7c2a7683b74
JackUhl <jackson.g.uhl@gmail.com>
2025-04-27 16:27:12 -0400

    Remove home reveal component

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -9,9 +9,7 @@ import github from "../../assets/svg/github.svg"
 import { emailAddress, githubUrl, linkedInUrl } from "../../models/constants/ExternalUrlConstants";
 import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
 import resume from "../../assets/docs/ResumePage.pdf"
-import RevealComponent from "../../components/RevealComponent/RevealComponent";
 import { WindowStyle } from "../../models/enums/WindowStyles";
-import { RevealTimeoutInMs } from "../../models/constants/ConfigurationConstants";
 
 export default function Home() {
     const [randomWindowStyle] = useState<WindowStyle>(Math.floor(Math.random() * Object.keys(WindowStyle).length));
@@ -19,31 +17,29 @@ export default function Home() {
 
     return (
         <div className={isMobile ? mobileHomeContainer : desktopHomeContainer}>
-            <RevealComponent timeoutInterval={RevealTimeoutInMs}>
-                <h1>Hello,<br/>I Write Computer Code</h1>
-                <TerminalComponent
-                    text={"Hi, my name is Jackson Uhl, welcome to my personal website! As an exceptionally driven and motivated junior software developer, I bring a strong work ethic to the team. Despite only having a couple years of enterprise level development experience, I have successfully demonstrated my ability to deliver high-quality, scalable solutions across both front-end and back-end development as a full-stack developer. Taking advantage of exciting new technologies and best practices, I strive to deliver an application that meets business requirements and is simultaneously engaging to the end users."}
-                    theme={randomWindowStyle}
-                />
-                <div className={classNameJoin([contactContainer, flexRow, justifyContentBetween, alignItemsCenter, flexWrap, rowGap])}>
-                    <div className={classNameJoin([flexRow, alignItemsCenter, columnGap])}>
-                        <ButtonComponent 
-                            buttonElement={<p>View CV</p>}
-                            href={resume}
-                            openInNewTab={true}
-                        />
-                        <ButtonComponent 
-                            buttonElement={<p>Contact Me</p>}
-                            href={`mailto:${emailAddress}`}
-                            openInNewTab={false}
-                        />
-                    </div>
-                    <div className={classNameJoin([flexRow, alignItemsCenter, columnGap])}>
-                        <a href={linkedInUrl} target="_blank"><img src={linkedin} className={contactIcon}/></a>
-                        <a href={githubUrl} target="_blank"><img src={github} className={contactIcon}/></a>
-                    </div>
+            <h1>Hello,<br/>I Write Computer Code</h1>
+            <TerminalComponent
+                text={"Hi, my name is Jackson Uhl, welcome to my personal website! As an exceptionally driven and motivated junior software developer, I bring a strong work ethic to the team. Despite only having a couple years of enterprise level development experience, I have successfully demonstrated my ability to deliver high-quality, scalable solutions across both front-end and back-end development as a full-stack developer. Taking advantage of exciting new technologies and best practices, I strive to deliver an application that meets business requirements and is simultaneously engaging to the end users."}
+                theme={randomWindowStyle}
+            />
+            <div className={classNameJoin([contactContainer, flexRow, justifyContentBetween, alignItemsCenter, flexWrap, rowGap])}>
+                <div className={classNameJoin([flexRow, alignItemsCenter, columnGap])}>
+                    <ButtonComponent 
+                        buttonElement={<p>View CV</p>}
+                        href={resume}
+                        openInNewTab={true}
+                    />
+                    <ButtonComponent 
+                        buttonElement={<p>Contact Me</p>}
+                        href={`mailto:${emailAddress}`}
+                        openInNewTab={false}
+                    />
                 </div>
-            </RevealComponent>
+                <div className={classNameJoin([flexRow, alignItemsCenter, columnGap])}>
+                    <a href={linkedInUrl} target="_blank"><img src={linkedin} className={contactIcon}/></a>
+                    <a href={githubUrl} target="_blank"><img src={github} className={contactIcon}/></a>
+                </div>
+            </div>
         </div>
     )
 }
