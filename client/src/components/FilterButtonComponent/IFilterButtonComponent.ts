286f7a1e2209b4ff5c83dc6f722f3e386357a97c
JackUhl <jackson.g.uhl@gmail.com>
2025-04-27 16:21:43 -0400

    Update filter buttons component

--- client/src/components/FilterButtonComponent/IFilterButtonComponent.ts
+++
@@ -1,5 +0,0 @@
-export default interface IFilterButtonComponent {
-    title: string;
-    selectedBlogTags: string[];
-    setSelectedBlogTags: (tags: string[]) => void;
-}
--- client/src/pages/Blog/Blog.tsx
+++ client/src/pages/Blog/Blog.tsx
@@ -1,43 +1,25 @@
-import { useCallback, useContext, useEffect, useMemo, useState } from "react";
-import FilterButtonComponent from "../../components/FilterButtonComponent/FilterButtonComponent";
+import { useContext, useMemo, useState } from "react";
 import { useFetch } from "../../hooks/useFetch";
 import { LoadingState } from "../../models/enums/LoadingState";
 import { BlogService } from "../../services/BlogService";
-import { alignItemsCenter, flexColumn, flexRow, flexWrap, justifyContentCenter, rowGap } from "../../styling/shared.module.css";
+import { flexColumn, justifyContentCenter, rowGap } from "../../styling/shared.module.css";
 import { classNameJoin } from "../../utilities/helpers/ClassnameJoiner";
 import Loading from "../Loading/Loading";
-import { blogFilterGap, blogClearFilters, mobileBlogContainer, desktopBlogContainer } from "./Blog.module.css";
+import { mobileBlogContainer, desktopBlogContainer } from "./Blog.module.css";
 import RevealComponent from "../../components/RevealComponent/RevealComponent";
 import { IsMobileContext } from "../../contexts/IsMobileContext";
 import BlogCardComponent from "../../components/BlogCardComponent/BlogCardComponent";
 import Failed from "../Failed/Failed";
 import { RevealTimeoutInMs } from "../../models/constants/ConfigurationConstants";
+import FilterButtonsComponent from "../../components/FilterButtonsComponent/FilterButtonsComponents";
+import { BlogItem } from "../../models/objects/BlogItem";
 
 export default function Blog() {
-    const [allBlogTags, setAllBlogTags] = useState<string[]>([]);
     const [selectedBlogTags, setSelectedBlogTags] = useState<string[]>([]);
 
     const isMobile = useContext(IsMobileContext);
     const serviceCall = useMemo(() => BlogService.GetAllBlogs(), []);
     const fetch = useFetch(serviceCall);
-
-    useEffect(() => {
-        if(fetch.response) {
-            let tags : string[] = [];
-            fetch.response.forEach((blog) => {
-                blog.tags.forEach((tag) => {
-                    if(!tags.includes(tag)) {
-                        tags.push(tag);
-                    }
-                })
-            });
-            setAllBlogTags(tags);
-        }
-    }, [fetch.response])
-
-    const resetSelectedBlogTags = useCallback(() => {
-        setSelectedBlogTags([]);
-    }, []);
 
     if(fetch.loadingState == LoadingState.loading) {
         return <Loading/>
@@ -54,12 +36,7 @@ export default function Blog() {
     return (
         <div className={classNameJoin([flexColumn, justifyContentCenter, rowGap, isMobile ? mobileBlogContainer : desktopBlogContainer])}>
             <RevealComponent timeoutInterval={RevealTimeoutInMs}>
-                <div className={classNameJoin([flexRow, alignItemsCenter, flexWrap, blogFilterGap])}>
-                    {allBlogTags.map((blogTag, index) => (
-                        <FilterButtonComponent title={blogTag} selectedBlogTags={selectedBlogTags} setSelectedBlogTags={setSelectedBlogTags} key={index}/>
-                    ))}
-                    {selectedBlogTags.length > 0 && <p className={blogClearFilters} onClick={resetSelectedBlogTags}>Clear Filters</p>}
-                </div>
+                <FilterButtonsComponent allBlogs={fetch.response as BlogItem[]} selectedBlogTags={selectedBlogTags} setSelectedBlogTags={setSelectedBlogTags}/>
                 {filteredBlogs?.map((blogItem, index) => (
                     <BlogCardComponent blogItem={blogItem} key={index}/>
                 ))}
