8bfd4ece72a95fcde133c8b6efeb4847b614001d
JackUhl <jackson.g.uhl@gmail.com>
2025-05-01 23:16:58 -0400

    Added server side caching

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import NodeCache from 'node-cache';
 import { apiRoute, blogIdParam, blogRoute, resumeRoute } from './routes';
 import { blogItems } from './constants/BlogItems';
 import { resumeItems } from './constants/ResumeItems';
@@ -8,34 +9,63 @@ const app = express();
 const port = 5000;
 const clientDistPath = path.join(__dirname, '..', '..', 'client', 'dist');
 const clientDistFile = "index.html"
+const cacheTimeout = 600;
+const cache = new NodeCache({stdTTL: cacheTimeout});
 
 app.use(express.static(clientDistPath));
 
 app.get(apiRoute, (req, res) => {
-  res.json("Test");
+  res.json("Api");
 });
 
 app.get(path.posix.join(apiRoute, resumeRoute), (req, res) => {
-  res.status(200).send(resumeItems)
+  let cacheKey = req.originalUrl;
+  let cachedValue = cache.get(cacheKey);
+
+  if(cachedValue) {
+    res.json(cachedValue);
+    return;
+  }
+
+  cache.set(cacheKey, resumeItems);
+  res.json(resumeItems);
 })
 
 //Endpoint to get all blog listings (does not include content data)
 app.get(path.posix.join(apiRoute, blogRoute), (req, res) => {
+  let cacheKey = req.originalUrl;
+  let cachedValue = cache.get(cacheKey);
+
+  if(cachedValue) {
+    res.json(cachedValue);
+    return;
+  }
+
   const strippedBlogItems = blogItems.map((blogItem) => {
     const { content, ...rest } = blogItem;
     return rest;
   });
 
-  res.status(200).json(strippedBlogItems);
+  cache.set(cacheKey, cachedValue);
+  res.json(strippedBlogItems);
 });
 
 //Endpoint to get a specific blog listing (includes content)
 app.get(path.posix.join(apiRoute, blogRoute, blogIdParam), (req, res) => {
+  let cacheKey = req.originalUrl;
+  let cachedValue = cache.get(cacheKey);
+
+  if(cachedValue) {
+    res.json(cachedValue);
+    return;
+  }
+  
   const blogId = parseInt(req.params.blogId);
   const blogItem = blogItems.find(item => item.id === blogId);
 
   if (blogItem) {
-    res.status(200).json(blogItem);
+    cache.set(cacheKey, blogItem)
+    res.json(blogItem);
   } else {
     res.status(404).send();
   }
