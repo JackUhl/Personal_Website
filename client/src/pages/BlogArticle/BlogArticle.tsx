89e0ddf76b283bdd8fb4d62866bcd203aaeac456
Jackson Uhl <jackson.g.uhl@gmail.com>
2025-06-14 16:48:30 -0400

    Update blog article to include back arrow

--- client/src/pages/BlogArticle/BlogArticle.tsx
+++ client/src/pages/BlogArticle/BlogArticle.tsx
@@ -1,10 +1,10 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { useFetch } from "../../hooks/useFetch";
 import { BlogService } from "../../services/BlogService";
 import { LoadingState } from "../../models/enums/LoadingState";
 import Loading from "../Loading/Loading";
 import { useMemo } from "react";
-import { blogArticleContent, blogArticleDate, blogArticleTitle, desktopBlogArticleContainer, mobileBlogArticleContainer } from "./BlogArticle.module.css";
+import { blogArticleContent, blogArticleDate, blogArticleReturnArrow, blogArticleTitle, desktopBlogArticleContainer, mobileBlogArticleContainer } from "./BlogArticle.module.css";
 import { classNameJoin } from "../../utilities/helpers/ClassnameJoiner";
 import { alignItemsCenter, flexColumn, flexRow, justifyContentCenter } from "../../styling/shared.module.css";
 import RevealComponent from "../../components/RevealComponent/RevealComponent";
@@ -13,6 +13,8 @@ import ContentSwitcherComponent from "../../components/ContentSwitcherComponent/
 import { renderPartialDate } from "../../utilities/helpers/DateRenderer";
 import Failed from "../Failed/Failed";
 import { useIsMobile } from "../../hooks/useIsMobile";
+import { BlogRoute } from "../../models/constants/RouteConstants";
+import arrowIcon from "../../assets/svg/arrow.svg";
 
 export default function BlogArticle() {
     const {id} = useParams();
@@ -34,6 +36,12 @@ export default function BlogArticle() {
     return (
         <div className={isMobile ? mobileBlogArticleContainer : desktopBlogArticleContainer}>
             <RevealComponent>
+                <Link
+                    to={BlogRoute}
+                    className={classNameJoin([flexRow, alignItemsCenter])}
+                >
+                    <img src={arrowIcon} className={blogArticleReturnArrow}/><span>Back to Blogs</span>
+                </Link>
                 <div className={classNameJoin([flexColumn, alignItemsCenter])}>
                     <p className={blogArticleTitle}>{response.title}</p>
                     <p className={blogArticleDate}>{renderPartialDate(new Date(response.createdDate))}</p>
