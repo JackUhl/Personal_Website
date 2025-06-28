eaeae2ce6d5d2ad41125b06d01bc9fe9ebd68407
JackUhl <jackson.g.uhl@gmail.com>
2025-06-28 09:38:48 -0400

    Update styling

--- client/src/components/ResourcesContentComponent/ResourcesContentComponent.tsx
+++ client/src/components/ResourcesContentComponent/ResourcesContentComponent.tsx
@@ -1,11 +1,11 @@
-import { alignItemsCenter, flexColumn, justifyContentCenter, rowGapSmall } from "../../styling/shared.module.css";
+import { alignItemsCenter, flexColumn, justifyContentCenter } from "../../styling/shared.module.css";
 import { classNameJoin } from "../../utilities/helpers/ClassnameJoiner";
 import IResourcesContentComponent from "./IResourcesContentComponent";
 import { linkContent, resourcesTitle } from "./ResourcesContentComponent.module.css";
 
 export default function ResourcesContentComponent(props: IResourcesContentComponent) {
     return (
-        <div className={classNameJoin([flexColumn, justifyContentCenter, alignItemsCenter, rowGapSmall])}>
+        <div className={classNameJoin([flexColumn, justifyContentCenter, alignItemsCenter])}>
             <span className={resourcesTitle}>Resources</span>
             {props.resources.map((resource, index) => (
                 <div key={index}>
