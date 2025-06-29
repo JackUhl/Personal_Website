0deb72fe2c47f6acc3ff07c90983dc6b56d3550f
JackUhl <jackson.g.uhl@gmail.com>
2025-06-29 16:52:18 -0400

    Update links to open in new window and add gap

--- client/src/components/ResourcesContentComponent/ResourcesContentComponent.tsx
+++ client/src/components/ResourcesContentComponent/ResourcesContentComponent.tsx
@@ -1,15 +1,15 @@
 import { alignItemsCenter, flexColumn, justifyContentCenter } from "../../styling/shared.module.css";
 import { classNameJoin } from "../../utilities/helpers/ClassnameJoiner";
 import IResourcesContentComponent from "./IResourcesContentComponent";
-import { linkContent, resourcesTitle } from "./ResourcesContentComponent.module.css";
+import { linkContent, resourcesContainer, resourcesTitle } from "./ResourcesContentComponent.module.css";
 
 export default function ResourcesContentComponent(props: IResourcesContentComponent) {
     return (
-        <div className={classNameJoin([flexColumn, justifyContentCenter, alignItemsCenter])}>
+        <div className={classNameJoin([flexColumn, justifyContentCenter, alignItemsCenter, resourcesContainer])}>
             <span className={resourcesTitle}>Resources</span>
             {props.resources.map((resource, index) => (
                 <div key={index}>
-                    <a href={resource.link} className={linkContent}>{resource.resource}</a>
+                    <a href={resource.link} target="_blank" className={linkContent}>{resource.resource}</a>
                 </div>
             ))}
         </div>
