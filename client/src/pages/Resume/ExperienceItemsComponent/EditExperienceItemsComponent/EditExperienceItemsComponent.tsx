da9f05a06e77abc7fa837539ab8250966b8fb4c7
JackUhl <jackson.g.uhl@gmail.com>
2025-12-13 10:48:51 -0500

    Move css

--- client/src/pages/Resume/ExperienceItemsComponent/EditExperienceItemsComponent/EditExperienceItemsComponent.tsx
+++ client/src/pages/Resume/ExperienceItemsComponent/EditExperienceItemsComponent/EditExperienceItemsComponent.tsx
@@ -3,7 +3,7 @@ import { IEditExperienceItemsComponent } from "./IEditExperienceItemsComponent";
 import { alignItemsCenter, flexColumn, flexGrow, flexRow, fullWidth, justifyContentCenter } from "../../../../styling/shared.module.css";
 import TextInputComponent from "../../../../components/InputComponents/TextInputComponent/TextInputComponent";
 import { classNameJoin } from "../../../../utilities/helpers/ClassnameJoiner";
-import { closeIcon, closeIconSpacing, container, label, plusIcon } from "./EditExperienceItemsComponent.module.css";
+import { closeIcon, closeIconSpacing, container, field, label, plusIcon } from "./EditExperienceItemsComponent.module.css";
 import TextAreaInputComponent from "../../../../components/InputComponents/TextAreaInputComponent/TextAreaInputComponent";
 import DateInputComponent from "../../../../components/InputComponents/DateInputComponent/DateInputComponent";
 import closeSvg from "../../../../assets/svg/close.svg";
@@ -70,54 +70,50 @@ export default function EditExperienceItemsComponent(props: IEditExperienceItems
     return (
         <>
             {props.experienceItems.map((experienceItem, experienceItemIndex) => (
-                <div key={experienceItemIndex} className={classNameJoin([flexColumn, fullWidth])}>
-                    <div className={classNameJoin([flexRow, flexGrow, container])}>
-                        <div className={flexGrow}>
-                            <TextInputComponent
-                                label="Main Text"
-                                required={true}
-                                value={experienceItem.mainText}
-                                onChange={(event) => handleMainTextChange(event, experienceItemIndex)}
-                            />
-                        </div>
-                        <div className={flexGrow}>
-                            <TextInputComponent
-                                label="Location"
-                                required={true}
-                                value={experienceItem.subText}
-                                onChange={(event) => handleSubTextChange(event, experienceItemIndex)}
-                            />
-                        </div>
+                <div key={experienceItemIndex} className={classNameJoin([flexColumn, fullWidth, container, field])}>
+                    <div className={classNameJoin([flexGrow, field])}>
+                        <TextInputComponent
+                            label="Main Text"
+                            required={true}
+                            value={experienceItem.mainText}
+                            onChange={(event) => handleMainTextChange(event, experienceItemIndex)}
+                        />
                     </div>
-                    <div className={classNameJoin([flexRow, flexGrow, container])}>
-                        <div className={flexGrow}>
-                            <TextInputComponent
-                                label="Position"
-                                required={false}
-                                value={experienceItem.position}
-                                onChange={(event) => handlePositionTextChange(event, experienceItemIndex)}
-                            />
-                        </div>
-                        <div className={flexGrow}>
-                            <DateInputComponent
-                                label="Start Date"
-                                required={true}
-                                value={experienceItem.start}
-                                onChange={(event) => handleStartTextChange(event, experienceItemIndex)}
-                            />
-                        </div>
-                        <div className={flexGrow}>
-                            <DateInputComponent
-                                label="End Date"
-                                required={false}
-                                value={experienceItem.end}
-                                onChange={(event) => handleEndTextChange(event, experienceItemIndex)}
-                            />
-                        </div>
+                    <div className={classNameJoin([flexGrow, field])}>
+                        <TextInputComponent
+                            label="Location"
+                            required={true}
+                            value={experienceItem.subText}
+                            onChange={(event) => handleSubTextChange(event, experienceItemIndex)}
+                        />
+                    </div>
+                    <div className={classNameJoin([flexGrow, field])}>
+                        <TextInputComponent
+                            label="Position"
+                            required={false}
+                            value={experienceItem.position}
+                            onChange={(event) => handlePositionTextChange(event, experienceItemIndex)}
+                        />
+                    </div>
+                    <div className={classNameJoin([flexGrow, field])}>
+                        <DateInputComponent
+                            label="Start Date"
+                            required={true}
+                            value={experienceItem.start}
+                            onChange={(event) => handleStartTextChange(event, experienceItemIndex)}
+                        />
+                    </div>
+                    <div className={classNameJoin([flexGrow, field])}>
+                        <DateInputComponent
+                            label="End Date"
+                            required={false}
+                            value={experienceItem.end}
+                            onChange={(event) => handleEndTextChange(event, experienceItemIndex)}
+                        />
                     </div>
                     <p className={label}>Description</p>
                     {experienceItem.description.map((descriptionItem, descriptionItemIndex) =>
-                        <div key={descriptionItemIndex} className={classNameJoin([flexRow, alignItemsCenter, container])}>
+                        <div key={descriptionItemIndex} className={classNameJoin([flexRow, alignItemsCenter, field])}>
                             <TextAreaInputComponent
                                 required={false}
                                 value={descriptionItem}
@@ -135,7 +131,7 @@ export default function EditExperienceItemsComponent(props: IEditExperienceItems
                             </div>
                         </div>
                     )}
-                    <div className={classNameJoin([flexRow, justifyContentCenter, container])}>
+                    <div className={classNameJoin([flexRow, justifyContentCenter, field])}>
                         <OnClickButtonComponent
                             buttonElement={
                                 <div className={classNameJoin([flexRow])}>
