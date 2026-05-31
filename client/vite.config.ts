d08b5334be2de87501fc6c4a71420214ffedd42e
JackUhl <jackson.g.uhl@gmail.com>
2026-05-31 16:57:45 -0400

    Add vitest support

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -10,6 +11,12 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '/api': env.VITE_PROXY_URL
       },
+    },
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './src/test/setup.ts',
+      css: true,
     },
   };
 });
