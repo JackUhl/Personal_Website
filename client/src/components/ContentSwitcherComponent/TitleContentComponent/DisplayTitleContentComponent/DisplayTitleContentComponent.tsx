368bbe241c67ca837bbed59fd846eeb8e86e5fa3
JackUhl <jackson.g.uhl@gmail.com>
2026-07-16 19:46:08 -0400

    Fix bug with title copying

--- client/src/components/ContentSwitcherComponent/TitleContentComponent/DisplayTitleContentComponent/DisplayTitleContentComponent.tsx
+++ client/src/components/ContentSwitcherComponent/TitleContentComponent/DisplayTitleContentComponent/DisplayTitleContentComponent.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useState } from "react";
 
+import checkmarkSvg from "../../../../assets/svg/checkmark.svg"
 import linkSvg from "../../../../assets/svg/link.svg";
 import { alignItemsCenter, flexRow, icon, justifyContentCenter } from "../../../../styling/shared.module.css";
 import { classNameJoin } from "../../../../utilities/helpers/ClassnameJoiner/ClassnameJoiner";
@@ -8,20 +9,37 @@ import IDisplayTitleContentComponent from "./IDisplayTitleContentComponent";
 
 export default function DisplayTitleContentComponent(props: IDisplayTitleContentComponent) {
     const [isHovering, setIsHovering] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
 
     const elementId = useMemo(() => {
         return props.title.toLowerCase().trim().replace(/\s+/g, "_");
     }, [props.title]);
 
     const handleTitleClick = () => {
-        navigator.clipboard.writeText(`${window.location.href}#${elementId}`);
+        setIsChecked(true);
+        const fragmentIndex = window.location.href.indexOf('#');
+        if(fragmentIndex != -1) {
+            navigator.clipboard.writeText(`${window.location.href.substring(0, fragmentIndex)}#${elementId}`);
+        }
+        else {
+            navigator.clipboard.writeText(`${window.location.href}#${elementId}`);
+        }
+    }
+
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    }
+
+    const handleMouseOut = () => {
+        setIsHovering(false);
+        setIsChecked(false);
     }
 
     return (
         <div className={classNameJoin([flexRow, justifyContentCenter, alignItemsCenter])}>
             <div
-                onMouseOver={() => setIsHovering(true)}
-                onMouseOut={() => setIsHovering(false)}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
                 className={titleContainer}
             >
                 <p
@@ -31,7 +49,7 @@ export default function DisplayTitleContentComponent(props: IDisplayTitleContent
                 >
                     {props.title}
                 </p>
-                {isHovering && <img src={linkSvg} className={classNameJoin([linkIcon, icon])} />}
+                {isHovering ? (isChecked ? <img src={checkmarkSvg} className={classNameJoin([linkIcon, icon])} /> : <img src={linkSvg} className={classNameJoin([linkIcon, icon])} />) : null}
             </div>
         </div>
     );
